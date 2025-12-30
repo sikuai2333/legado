9ce76142d64c271cc8ebf32462942d0b6aed8a2c
sikuai <2643927725@qq.com>
2025-12-30 11:09:32 +0800

    删除qq群和频道

--- app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
@@ -119,24 +119,6 @@ class AboutFragment : PreferenceFragmentCompat() {
         }
     }
 
-    /**
-     * 加入qq群
-     */
-    private fun joinQQGroup(key: String): Boolean {
-        val intent = Intent()
-        intent.data =
-            Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D$key")
-        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面
-        // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        kotlin.runCatching {
-            startActivity(intent)
-            return true
-        }.onFailure {
-            toastOnUi("添加失败,请手动添加")
-        }
-        return false
-    }
-
     private fun saveLog() {
         Coroutine.async {
             val backupPath = AppConfig.backupPath ?: let {
